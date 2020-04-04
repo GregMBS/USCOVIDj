2a3236fd2341f87f91747373afaa4a39af635dd8
Greg Blumenthal <greg@gmbs-consulting.com>
2020-04-04 14:51:19 -0500

    Just one hit on NYTimes

--- covid_chart.js
+++ covid_chart.js
@@ -16,49 +16,52 @@ function sumByDate(obj) {
 	return result;
 }
 $(function() {
-	getCharts('all');
-	$('#states').change(function() {
-		getCharts($('#states').val())
-		});
+	$.ajax({
+		  dataType: 'text',
+			async: false,
+		  url: "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv",
+		  success: function( csvd ) {
+				var data = $.csv.toObjects(csvd);
+				var text = JSON.stringify(data);
+				$('#data').val(text);
+				getCharts('all', data);
+				$('#states').change(function() {
+					  data = JSON.parse($('#data').val());
+						getCharts($('#states').val(), data)
+				});
+			}
+	  });
 });
-function getCharts(mystate) {
+function getCharts(mystate, result) {
 	const formatter = new Intl.DateTimeFormat('en-us', { month: 'short', day: 'numeric' });
 	var dates=[];
 	var cases=[];
 	var deaths=[];
-	$.ajax({
-	  dataType: 'text',
-	  url: "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv",
-	  success: function( csvd ) {
-		var result = $.csv.toObjects(csvd);
-        // var result = JSON.stringify(items);
-		$('#states').empty();
-		const stateList = [...new Set(result.map(item => item.state))].sort();
-		var option = '<option value="all">US</option>';
-		for (var i=0;i<stateList.length;i++){
-		  option += '<option value="'+ stateList[i] + '">' + stateList[i] + '</option>';
-		}
-		$('#states').append(option);
-		var label = mystate;
-		if (mystate == "all") {
-			var output=sumByDate(result);
-			label = 'US';
-		} else {
-			var interim=result.
-				filter(function (x) { 
-					return x.state == mystate; 
-				});
-			var output=sumByDate(interim);
-		}
-		$('#covid').html(output);
-		output.forEach(function (item) {
-			var d1 = new Date(item.date);
-			var f1 = formatter.format(d1);
-			dates.push(f1);
-			cases.push(parseInt(item.cases));
-			deaths.push(parseInt(item.deaths));
-		});
-		graph(label,dates,cases,deaths);
-	  }
-	});	
+	$('#states').empty();
+	const stateList = [...new Set(result.map(item => item.state))].sort();
+	var option = '<option value="all">US</option>';
+	for (var i=0;i<stateList.length;i++){
+	  option += '<option value="'+ stateList[i] + '">' + stateList[i] + '</option>';
 	}
+	$('#states').append(option);
+	var label = mystate;
+	if (mystate == "all") {
+		var output=sumByDate(result);
+		label = 'US';
+	} else {
+		var interim=result.
+			filter(function (x) {
+				return x.state == mystate;
+			});
+		var output=sumByDate(interim);
+	}
+	$('#covid').html(output);
+	output.forEach(function (item) {
+		var d1 = new Date(item.date);
+		var f1 = formatter.format(d1);
+		dates.push(f1);
+		cases.push(parseInt(item.cases));
+		deaths.push(parseInt(item.deaths));
+	});
+	graph(label,dates,cases,deaths);
+}
